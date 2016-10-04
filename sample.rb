0c7f24b8c6726e7c07aac5af9c1c00be9f43f3e4
Mark <markchav3z@gmail.com>
2016-10-04 18:17:25 +0800

    init

---
+++ sample.rb
@@ -0,0 +1,60 @@
+require 'json'
+require 'net/http'
+require 'uri'
+
+
+email = ARGV.empty? ? "billy.nicart@gmail.com" : ARGV.first
+URL = "http://msme.herokuapp.com/api/v1/themes?email=#{email}"
+
+class Jarvis
+  attr_reader :filename, :url
+
+  def initialize(filename, url)
+    @filename = filename
+    @url = url
+  end
+
+  def process
+    write_to_primary
+    write_to_secondary
+    write_to_tertiary
+  end
+
+  private
+
+  def write_to_primary
+    File.write(f = filename, File.read(f).gsub(/\$primary: .*/,"$primary: #{fetch_variables[:primary]};"))
+  end
+
+  def write_to_secondary
+    File.write(f = filename, File.read(f).gsub(/\$secondary: .*/,"$secondary: #{fetch_variables[:secondary]};"))
+  end
+
+  def write_to_tertiary
+    File.write(f = filename, File.read(f).gsub(/\$tertiary: .*/,"$tertiary: #{fetch_variables[:tertiary]};"))
+  end
+
+  def fetch_variables
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body).first
+
+    {
+      primary: result["primary"],
+      secondary: result["secondary"],
+      tertiary: result["tertiary"]
+    }
+
+  rescue => e
+    { primary: "#FFF", secondary: "#FFF", tertiary: "#FFF" }
+  end
+
+  def uri
+    URI.parse(url)
+  end
+end
+
+# START OP
+jarvis = Jarvis.new("app.variables.scss", URL)
+jarvis.process
+puts "DONE"
+
